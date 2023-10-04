e66059fa3ccbc1b95b1ac90de0e9b233be45536f
sarai2525 <s.arai@newell-productions.com>
2023-10-04 13:54:28 +0900

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/prettier",
+  ],
+  plugins: [],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/no-v-html": "off",
+  },
+};
---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  ssr: true,
+  srcDir: 'src',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#',
+      },
+      title: '',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'manifest', href: '/manifest.webmanifest' }],
+    },
+  },
+  imports: {
+    autoImport: false,
+  },
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: true,
+      failOnError: false,
+    },
+  },
+  telemetry: false,
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module'],
+  devtools: { enabled: true },
+  runtimeConfig: {},
+})
