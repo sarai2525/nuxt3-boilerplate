0804f1aaee314b5da8a14825b918adc342b237ac
sarai <s.arai@newell-productions.com>
2023-10-08 10:26:31 +0900

    vscode setting & add postcss-scss (#5)

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,4 +2,11 @@ module.exports = {
   extends: ['stylelint-config-recommended-vue'],
   ignoreFiles: ['**/node_modules/**'],
   rules: {},
+  customSyntax: 'postcss-scss',
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      customSyntax: 'postcss-html',
+    },
+  ],
 }
