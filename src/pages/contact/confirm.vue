c7b9016fe5151a425afc9bd49552f993686c259c
sharray2918 <2010068043@campus.ouj.ac.jp>
2024-03-13 23:18:35 +0900

    cretae: confirm page

---
+++ src/pages/contact/confirm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>お問い合わせ 入力内容確認</h1>
+    <section class="section">
+      <TheContactFormConfirm class="contact-form" />
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+  import { useMeta } from '@/composables/useMeta'
+  useMeta({
+    ogTitle: 'お問い合わせ 入力内容確認',
+    ogSiteName: '',
+    ogDescription: '',
+    ogType: 'article',
+    ogLocale: 'ja_JP',
+    ogImage: '',
+    ogUrl: '',
+    robots: 'none',
+  })
+</script>
