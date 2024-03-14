fbd679aa3907cd018702292676ddbb81e00c3bb5
sharray2918 <156676782+sharray2918@users.noreply.github.com>
2024-03-14 12:46:59 +0900

    Fix env (#94)

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,19 @@
 <template>
   <footer class="footer">
-    <div class="footer-inner">Footer</div>
+    <div class="footer-inner">
+      Footer
+      <small v-if="appEnv === 'dev'">{{ appEnv }}</small>
+    </div>
   </footer>
 </template>
+<script lang="ts" setup>
+  const config = useRuntimeConfig()
+  const appEnv = config.public.APP_ENV
+</script>
+<style lang="scss" scoped>
+  small {
+    display: block;
+    font-size: 16px;
+    color: crimson;
+  }
+</style>
